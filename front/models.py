251698d9c5d677e3efd28bccbb4d19c84a950238
ARESP <sylvain.hebert@gmail.com>
2017-05-10 21:34:18 +0200

    template liste animal

--- front/models.py
+++ front/models.py
@@ -4,19 +4,22 @@ from django.utils.timezone import datetime
     
 
 class Animal(models.Model):
-    date_ajout =models.DateTimeField(auto_now_add=True)
     nom = models.CharField(max_length=200)
-    espece = (
+    espece_choix = (
             ('CAT', 'Chat'),
             ('DOG','Chien'),
         )
-    location = models.CharField(max_length=30)
+    espece = models.CharField(
+            max_length=3,
+            choices=espece_choix,
+            default='CAT',
+        )
     date_naissance = models.DateField(default=datetime.today)
     sexe_court_choice = (
             ('F', 'Femelle'),
             ('M', 'Male'),
         )
-    sexe_court = models.CharField(
+    sexe = models.CharField(
             max_length=1,
             choices=sexe_court_choice,
             default='F',
@@ -27,15 +30,16 @@ class Animal(models.Model):
             ('L','Grand'),
             ('XL','Très grand'),
         )
-    origine = models.CharField(max_length=200)
-    apparence = models.CharField(max_length=200)
     taille = models.CharField(
         max_length=2,
         choices=taille_choice,
         default='M',
-        )
+        )   
+    origine = models.CharField(max_length=200)
+    location = models.CharField(max_length=30) 
+    apparence = models.CharField(max_length=200)
     temperament = models.CharField(max_length=200)
-    num_puce = models.CharField(max_length=100)
+    num_puce = models.CharField(max_length=100, blank=True, null=True)
     statut_choice=(
             ('01','à adopter'),
             ('02','en attente'),
@@ -47,7 +51,14 @@ class Animal(models.Model):
         choices=statut_choice,
         default='01',
         )
-    description = models.TextField
+    description = models.TextField(blank=True,null=True)
+    date_ajout =models.DateTimeField(auto_now_add=True)
+    user_ajout = models.ForeignKey('auth.User',null=True)
+    date_publication = models.DateTimeField(blank=True,null=True,editable=False)
+
+    def publier(self):
+        self.date_publication=timezone.now()
+        self.save()
 
     def __str__(self):
         return self.nom
--- front/views.py
+++ front/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Animal
 
 # Create your views here.
 def animal_list(request):
-    return render(request, 'front/animal_list.html',{})
+    animals = Animal.objects.filter(date_publication__lte=timezone.now()).order_by('date_publication')
+    return render(request, 'front/animal_list.html',{'animals':animals})
