aadea60d9d371d01290cf371d74ec0a60e6a226f
Sylvain H <sylvain.hebert@gmail.com>
2017-05-11 11:47:55 +0200

    ebauche detail animal

--- front/urls.py
+++ front/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.animal_list, name='animal_list'),
+    url(r'^animal/(?P<pk>\d+)/$', views.animal_detail, name='animal_detail'),
 ]
--- front/views.py
+++ front/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Animal
 
@@ -6,3 +6,7 @@ from .models import Animal
 def animal_list(request):
     animals = Animal.objects.filter(date_publication__lte=timezone.now()).order_by('date_publication')
     return render(request, 'front/animal_list.html',{'animals':animals})
+
+def animal_detail(request,pk):
+	animal=get_object_or_404(Animal, pk=pk)
+	return render(request, 'front/animal_detail.html',{'animal':animal})
